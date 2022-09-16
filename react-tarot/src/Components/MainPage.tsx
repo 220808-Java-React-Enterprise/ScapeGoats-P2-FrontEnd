2e956cfa38da5e3bf5fe0ad4d9d50455bb9a0c78
J4JT <90707910+J4JT@users.noreply.github.com>
2022-09-16 18:16:27 -0400

    Added Login from david and added MainPage skeleton

---
+++ react-tarot/src/Components/MainPage.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Button from './Button'
+import Header from './Header'
+
+const MainPage = () => {
+  return (
+    <div>
+        <Header title="Reality Escape Cards Main Page" />
+        <Button text="Three Fates Divination" />
+        <br></br>
+        <Button text="Reading History" />
+        <br></br>
+        <Button text="Settings" />
+        <br></br>
+        <Button text="Exit" />
+        
+
+
+
+    </div>
+  )
+}
+
+export default MainPage
